49db0688f7afb0fe0cffe16398aa86ef84ea156e
Mannavard <mannavard.ak@gmail.com>
2018-09-04 15:35:36 +0300

    finish

--- src/app/article/article.component.ts
+++ src/app/article/article.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, HostBinding } from '@angular/core';
+import { Component, OnInit, HostBinding, Input } from '@angular/core';
 import { Article } from './article.model';
 
 @Component({
@@ -8,14 +8,11 @@ import { Article } from './article.model';
 })
 export class ArticleComponent implements OnInit {
 @HostBinding('attr.class') cssClass = 'row';
-article: Article;
+@Input() article: Article;
 
 constructor() {
-  this.article = new Article(
-    'Angular 2',
-    'http://angular.io',
-    10);
 }
+
 voteUp(): boolean {
 this.article.voteUp();
 return false;
@@ -24,7 +21,6 @@ voteDown(): boolean {
 this.article.voteDown();
 return false;
 }
-
 
 ngOnInit() {
 }
